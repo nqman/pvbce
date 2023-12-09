9f5680667e617551c0400949053229adf0f768a9
quangman <quangmank16@gmail.com>
2023-12-10 00:30:00 +0700

    home

---
+++ src/modules/home/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../components/Header";
+import Banner from "../components/Banner/Banner";
+import Services from "../components/Services/Services";
+import Media from "../components/Media/Media";
+import Awards from "../components/Awards/Awards";
+import Partners from "../components/Partners/Partners";
+import Footer from "../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Services />
+      <Media />
+      <Awards />
+      <Partners />
+      <Footer />
+    </>
+  );
+}
