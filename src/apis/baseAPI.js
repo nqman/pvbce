43060ca6cf367051f4db0350ebf60ba01650425b
Man Nguyen Quang <quangmank16@gmail.com>
2024-01-11 05:28:08 +0700

    https

--- src/apis/baseAPI.js
+++ src/apis/baseAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-  baseURL: "http://pvbce.io.vn/API",
+  baseURL: "https://pvbce.io.vn/API",
   //   headers: {
   //     TokenPVB:
   //       "oiMTcxNjUwODgwMDAwMCMCwiZXhwIjoxNzE2NjU2NDAwfQ.HsoestvkIN5Kub3jnAr8UddrPugJcxCsAm4QfMi4ZbU",
