d7651b842bc999dde1e9cc2a305f6369cf685ff8
Man Nguyen Quang <quangmank16@gmail.com>
2024-03-01 01:39:24 +0700

    updateCategory

--- src/apis/reportAPI.js
+++ src/apis/reportAPI.js
@@ -1,7 +1,25 @@
 import baseAPI from "./baseAPI";
 
+//Lấy danh sách hạng mục
+export async function getCategoriesAPI() {
+  try {
+    const resp = await baseAPI.get("categories");
+    return resp.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
 export async function addCategoryAPI(category) {
   try {
-    const resp = await baseAPI.post("category/save", category);
+    const formData = new FormData();
+    formData.append("id", 0);
+    formData.append("category", category);
+    const resp = await baseAPI.post(
+      "http://103.82.39.125:8080/categories/save",
+      formData
+    );
+    // console.log(resp)
   } catch (error) {}
 }
--- src/modules/report/components/Category.jsx
+++ src/modules/report/components/Category.jsx
@@ -1,63 +1,108 @@
-import { Button, Container, TextField } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { addCategoryAPI, getCategoriesAPI } from "../../../apis/reportAPI";
 
 export default function Category() {
   const [category, setCategory] = useState("");
+  const testCategories = [
+    { id: 1, value: "Nhật kí bảo dưỡng" },
+    { id: 2, value: "Nhật kí sửa chữa" },
+    { id: 3, value: "Chi phí khác" },
+  ];
   const handleChange = (e) => {
     // console.log(e.target.value);
     setCategory(e.target.value);
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(category);
+    // console.log(category);
     try {
       // await addCategoryAPI(category);
+      testCategories.push({ id: 0, value: category });
+      // console.log(testCategories);
+      setCategories(testCategories);
+
       setCategory("");
     } catch (error) {}
   };
+
+  const [categories, setCategories] = useState(testCategories);
+  useEffect(() => {
+    // setCategories(getCategoriesAPI());
+    console.log(categories);
+  }, [testCategories]);
   return (
     <div>
       <Container>
-        <div style={{ display: "flex", marginTop: "100px" }}>
+        <div
+          style={{
+            display: "flex",
+            marginTop: "50px",
+            justifyContent: "space-between",
+          }}
+        >
           <div>
-            <h3>Các hạng mục</h3>
-            <form onSubmit={handleSubmit}>
+            <h3>Thêm hạng mục</h3>
+            <form
+              style={{ display: "flex", alignItems: "center" }}
+              onSubmit={handleSubmit}
+            >
               <TextField
-                placeholder="Tên hạng mục"
+                label="Tên hạng mục"
                 id="outlined-size-small"
                 value={category}
                 sx={{ marginRight: "20px" }}
                 onChange={handleChange}
+                size="small"
+                style={{ width: "300px" }}
               />
-              <Button
-                variant="contained"
-                color="success"
+              <button
+                className="btn btn-primary"
                 // disabled={isLoading}
                 type="submit"
               >
                 Thêm
-              </Button>
+              </button>
             </form>
           </div>
           <div>
-            <table class="table table-bordered border-dark">
-              <thead class="table-light">
-                <tr>
-                  <th>STT</th>
-                  <th>Tên hạng mục</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+            <TableContainer
+              style={{ width: "500px", fontSize: "16px", textAlign: "center" }}
+            >
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      sx={{ border: 1, textAlign: "center" }}
+                      width={"50px"}
+                    >
+                      STT
+                    </TableCell>
+                    <TableCell sx={{ border: 1 }} align="center">
+                      CÁC HẠNG MỤC
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {categories?.map((category) => (
+                    <TableRow>
+                      <TableCell sx={{ border: 1 }}>{category.id}</TableCell>
+                      <TableCell sx={{ border: 1 }}>{category.value}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
         </div>
       </Container>
