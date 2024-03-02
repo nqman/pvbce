510cfd0122db4bab56668f51022726edce49f101
Man Nguyen Quang <quangmank16@gmail.com>
2024-03-03 00:10:44 +0700

    changeAPI

--- src/apis/reportAPI.js
+++ src/apis/reportAPI.js
@@ -3,7 +3,7 @@ import baseAPI from "./baseAPI";
 //Lấy danh sách hạng mục
 export async function getCategoriesAPI() {
   try {
-    const resp = await baseAPI.get("http://103.82.39.125:8080/categories");
+    const resp = await baseAPI.get("categories");
     return resp.data;
   } catch (error) {
     console.error(error);
@@ -18,18 +18,13 @@ export async function addCategoryAPI(category) {
     formData.append("id", 0);
     formData.append("name", category.name);
     formData.append("unit", category.unit);
-    const resp = await baseAPI.post(
-      "http://103.82.39.125:8080/categories/save",
-      formData
-    );
+    const resp = await baseAPI.post("categories/save", formData);
   } catch (error) {}
 }
 
 export async function deleteCategoryAPI(id) {
   try {
-    const resp = await baseAPI.get(
-      `http://103.82.39.125:8080/categories/delete/${id}`
-    );
+    const resp = await baseAPI.get(`categories/delete/${id}`);
     return resp.data;
   } catch (error) {
     throw error.response.data;
